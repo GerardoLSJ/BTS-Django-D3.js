b8acfab9ae65069be10b3533ddaccaf729f5877f
lsjgerardo <lsjgerardo@gmail.com>
2016-10-31 18:06:28 -0600

    recive objects

--- django-d3-example/play/urls.py
+++ django-d3-example/play/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 
 
-from .views import graph, data
+from .views import graph, data, get_name
 
 urlpatterns = [
     url(r'^$', graph),
     url(r'^api/data', data, name='data'),
+    url(r'^send/$',get_name ),
 ]
--- django-d3-example/play/views.py
+++ django-d3-example/play/views.py
@@ -6,12 +6,45 @@ from django.shortcuts import render
 from .models import Play
 
 
+#from django.shortcuts import render
+from django.http import HttpResponse
+
+from .forms import NameForm
+
+def get_name(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = NameForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            
+            return data(request,{'bar':456}) #HttpResponseRedirect('/thanks/')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = NameForm()
+
+    return render(request, 'graph/graph.html', {'form': form})
+
+
+
 def graph(request):
     return render(request, 'graph/graph.html')
 
 
-def data(request):
-    
+def data(request, data={}):
+    dic = {}
+    if request:
+        print(data)
+        dic = data
+    else:
+        dic = {}
+
+    '''
     dic = {
     "nodes": [
         {"id": "1", "group": 1},
@@ -34,7 +67,7 @@ def data(request):
 
     ]
     }
-
+    '''
     #nodes = [dict1,dict1,dict1,dict1,dict1]
 
     '''
@@ -47,4 +80,4 @@ def data(request):
         .values('month') \
         .annotate(count_items=Count('id'))
     '''
-    return JsonResponse((dic), safe=False)
+    return  HttpResponse(dic)#JsonResponse((dic), safe=False)
