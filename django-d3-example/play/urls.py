55aab0882c6ca996902a6348a228b4b07c2e96ae
lsjgerardo <lsjgerardo@gmail.com>
2016-11-13 08:49:24 -0600

    tidy code

--- django-d3-example/play/urls.py
+++ django-d3-example/play/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import url
 
 
-from .views import graph, data, get_name, readFrom, saveTo
+from .views import graph, data, get_tree, readFrom, saveTo
 
 urlpatterns = [
     url(r'^$', graph),
     url(r'^api/data', data, name='data'),
-    url(r'^send/$',get_name ),
+    url(r'^send/$',get_tree ),
     url(r'^api/read',readFrom),
     url(r'^api/save',saveTo),
 ]
--- django-d3-example/play/views.py
+++ django-d3-example/play/views.py
@@ -20,7 +20,7 @@ print('EJECUTANDO DESDE ARRIBA______')
 #This populates a global VAR named JSON.
 
 @csrf_exempt
-def get_name(request):
+def get_tree(request):
     # if this is a POST request we need to process the form data
     sanitized = []
     if request.method == 'POST':
@@ -83,58 +83,6 @@ def graph(request):
 
 
 def data(request, data={'void':'void'}):
-    print('en dataaaaa')
-    '''
-    dic = {}
-    if request:
-        print(data)
-        dic = data
-    else:
-        dic = {}
-
-    dic = {"nodes": [],"links": []}
-    
-    for item in JSON:
-        element = {'id':item['id'],'group':item['altura'] }
-        dic['nodes'].append(element)
-
-        if(item['hDer'] != 'None'): #Strin not None #FIX LATER WITH NILL
-            link = {'source': item['id'] , 'target':item['hDer']}
-            dic['links'].append(link)
-
-        if(item['hIzq'] != 'None'):
-            link = {'source': item['id'] , 'target':item['hIzq']}
-            dic['links'].append(link)
-            
-    print(dic)
-    request
-    '''
-
+    print('en data request')
 
     return JsonResponse([JSONlocal,volatil], safe=False)
-
-
-'''    
-    dic = {
-    "nodes": [
-        {"id": "1", "group": 1},
-        {"id": "2", "group": 2},
-        {"id": "3", "group": 2},
-        {"id": "4", "group": 3},
-        {"id": "5", "group": 3},
-        {"id": "6", "group": 3},
-        {"id": "7", "group": 3},
-        {"id": "8", "group": 4}
-
-    ],
-    "links": [
-        {"source": "1", "target": "2", "value": 1},
-        {"source": "1", "target": "3", "value": 8},
-        {"source": "2", "target": "4", "value": 1},
-        {"source": "2", "target": "5", "value": 1},
-        {"source": "3", "target": "6", "value": 3},
-        {"source": "3", "target": "7", "value": 8},
-
-    ]
-    }
-'''
