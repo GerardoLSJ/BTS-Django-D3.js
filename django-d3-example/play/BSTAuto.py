a14e849e4fb9ccc2f7614f72d75c206b4b84aa25
Braulio <braulioe697@gmail.com>
2016-11-13 00:07:10 -0600

    Search item

--- django-d3-example/play/BSTAuto.py
+++ django-d3-example/play/BSTAuto.py
@@ -11,6 +11,7 @@ class vertex:
         self.hizq = None
         self.hder = None
         self.altura = -1
+        self.profundidad = -1
         self.FE = 0
 
 class arbol:
@@ -88,15 +89,16 @@ class arbol:
             else:
                 hDer = act.hder.id
                 
-            print("Nodo: {:<7} FE: {:<3} Altura: {:<5} Padre: {:<7} hIzq: {:<7} hDer: {}".format(act.id,act.FE,act.altura,padre,hIzq,hDer))
+            print("Nodo: {:<7} FE: {:<3} Altura: {:<5} Profundidad: {:<5} Padre: {:<7} hIzq: {:<7} hDer: {}".format(act.id,act.FE,act.altura,act.profundidad,padre,hIzq,hDer))
 
             self.imprimir(act.hder)
 
-    def altura(self,act = False):	#Recorrido postorden,calcula la altura de las hojas a la raiz
+    def alturas(self,act = False,prof = 0):	#Recorrido postorden,calcula la altura de las hojas a la raiz, y la profundidad
         if act is False: act = self.raiz
         if act != None:
-            a1 = self.altura(act.hizq)
-            a2 = self.altura(act.hder)
+            act.profundidad = prof;
+            a1 = self.alturas(act.hizq,prof+1)
+            a2 = self.alturas(act.hder,prof+1)
             #print(max([a1,a2]))
             act.altura = max([a1,a2]) + 1
             return act.altura
@@ -120,7 +122,7 @@ class arbol:
     def imprimirDesv(self,arrDesv,act = False):
         if act is False: 
         	act = self.raiz
-        	self.altura()
+        	self.alturas()
         	self.calcFE()
         if act != None:
             self.imprimirDesv(arrDesv,act.hizq)
@@ -137,7 +139,7 @@ class arbol:
         if len(arrDesv) < 1:
 	        if act is False: 
 	        	act = self.raiz
-	        	self.altura()
+	        	self.alturas()
 	        	self.calcFE()
 	        if act != None:
 	            self.obtenerOBJDesv(arrDesv,act.hizq)
@@ -289,21 +291,25 @@ class arbol:
             
         #self.imprimir()
 
-    def buscarVertice(self,v, act = False):
+    def buscarVertice(self,v, act = False): #Retorna un arreglo, Encontrado:[1, objAct]; No encontrado:[0]
     	if act is False: 
     		act = self.raiz
 
     	if act != None:
-    		print("actual", act.id)
+    		#print("actual", act.id)
 	    	if act.id == v:
-	    		print("Vertice: ", act.id, "\t", "Profundidad: ",self.raiz.altura - act.altura)
+	    		#print("Vertice: ", act.id, "\t", "Profundidad: ", act.profundidad)
+	    		return [1,act]
 	    	else:
 	    		if act.id < v:
-	    			self.buscarVertice(v, act.hder)
+	    			retorno = self.buscarVertice(v, act.hder)
 	    		elif act.id > v:
-	    			self.buscarVertice(v,act.hizq)
+	    			retorno = self.buscarVertice(v,act.hizq)
     	else:
-    		print("Vertice {} no encontrado".format(v))
+    		#print("Vertice {} no encontrado".format(v))
+    		return [0]
+
+    	return retorno
 
 
 
@@ -322,6 +328,7 @@ class arbol:
 	    		#print(act.id)
 	    		act = act.hder
 	    	print("Elemento mayor:", act.id)
+
 
 
     def leerArchivo(self):
@@ -362,26 +369,39 @@ class main():
     a.imprimir()
 """
 
+#Ejemplo de busqueda de nodo
 """
 class main():
     #os.system("cls")
+    volatil = [11, 13, 16, 17, 19, 25, 27, 32, 33, 39, 43, 46, 48, 52, 54, 59, 62, 66, 75, 92, 95, 100, 102, 103, 105, 109, 111, 113, 124, 125, 127, 131, 137, 152, 155, 158, 164, 166, 175, 184, 185, 187, 193, 194, 201, 205, 208, 210, 211, 217, 237, 247, 251, 257, 259, 268, 272, 274, 276, 282, 284, 285,293, 297, 298, 301, 308, 317, 319, 328, 329, 337, 338, 339, 340, 354, 358, 374, 387, 388, 390, 399, 404, 413, 414, 420, 422, 430, 434, 437, 446, 448, 456, 468, 473, 480, 482, 483, 486, 493]
     a = arbol()
-    a.crearArbol([23, 54, 89, 39, 13, 36, 75, 14, 27,10,9,8,76,77,78,90])
+    a.crearArbol(volatil)
+    #a.crearArbol([23, 54, 89, 39, 13, 36, 75, 14, 27,10,9,8,76,77,78,90])
     #a.crearArbol([0,1,2,3,4,5,6,7,8,9])
     #a.crearArbol([10,9,8,7,6,5,4,3,2,1])
     #a.crearArbol([50,10,14])
     
     a.autobalanceo()
     a.imprimir()
-    x = 5
+    
+    x = 2
     while x:
     	x = x-1
-    	a.buscarVertice(int(input("Vertice a buscar: ")))
+    	vertice = int(input("Vertice a buscar: "))
+    	resultado = a.buscarVertice(vertice)
+    	if resultado[0] == 0:
+    		print("Vertice {} no encontrado".format(vertice))
+    	else:
+            nodo = resultado[1]
+            print("Nodo {} encontrado a una profundidad {} ".format(nodo.id,nodo.profundidad))
+            
+
 
     a.obtenerMax()
     a.obtenerMin()
+"""
 
-
+"""
     for i in range(5):
         #print("\n\n\nINSERTANDO EL NODO ", i)
         #a.insertarVertice(random.randrange(0,1000000))
